753c763486552e565ef8cdb195b8700b975a6a45
Cody Toney-Griffith <cody.toney@gmail.com>
2020-01-17 01:27:02 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,24 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
+<<<<<<< HEAD
   BRANDS.uniq = ["Uggs", "Rainbow", "Nike"]
   
+=======
+  BRANDS = []
+>>>>>>> 34ab2d52ce7dc4eb8ed58a46811ee704f28b3a3d
 
   def initialize(brand)
     @brand = brand 
     BRANDS << brand
+<<<<<<< HEAD
     brand.uniq = ["Uggs", "Rainbow", "Nike"]
     end
+=======
+    
+    
+  end
+>>>>>>> 34ab2d52ce7dc4eb8ed58a46811ee704f28b3a3d
 
   def cobble
     self.condition = "new"
